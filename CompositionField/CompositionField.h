1f5201b5e59aebf9ec12a86f359152439d87e195
mlebihan-accor <morgan.lebihan@accor.com>
2021-02-13 16:00:25 +0100

    feat(DynamicType) - DAPP-13406 - [POC] Create CompositionField franmework

---
+++ CompositionField/CompositionField.h
@@ -0,0 +1,18 @@
+//
+//  CompositionField.h
+//  CompositionField
+//
+//  Created by mlebihan on 13/02/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CompositionField.
+FOUNDATION_EXPORT double CompositionFieldVersionNumber;
+
+//! Project version string for CompositionField.
+FOUNDATION_EXPORT const unsigned char CompositionFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CompositionField/PublicHeader.h>
+
+
