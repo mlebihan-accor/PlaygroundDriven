46a1e4ee0d62b278b2f93ae71ac474e05e11145c
mlebihan-accor <morgan.lebihan@accor.com>
2021-02-26 14:43:07 +0100

    tech(TemplateForm) - DAPP-13409 - [POC] Create full width template

---
+++ Form/FormTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FormTableViewCell.swift
+//  Form
+//
+//  Created by mlebihan on 26/02/2021.
+//
+
+import UIKit
+
+class FormTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
